3131181a241e913627c659de1e4bd6ea234a8faf
delindell <daviselindell@gmail.com>
2020-05-09 13:11:32 -0500

    setup done

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>INSIDE THE APPPP COMPPP</h2>
+        <button className="btn btn-primary">CLICK CLACK</button>
+      </div>
+     );
+  }
+}
+
+export default App;
